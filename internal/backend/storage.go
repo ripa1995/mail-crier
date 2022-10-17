7d5dab5ee917e78b4fa85ddbf882b3b7ef886720
ripa1995 <roberto.ripamonti@ymail.com>
2022-10-17 12:14:55 +0200

    feat: basic storage structure

--- internal/backend/storage.go
+++ internal/backend/storage.go
@@ -1,1 +1,27 @@
-package backend;
+package backend
+
+import (
+	"time"
+	"github.com/google/uuid"
+)
+
+type MailingList struct {
+	DisplayName string
+	Topic string
+}
+
+type Subscriber struct {
+	Name    string
+	Surname string
+	DisplayName string
+	Email string
+}
+
+type Subscription struct {
+	Id uuid.UUID
+	MailingList *MailingList
+	Subscriber *Subscriber
+	SubscriptionDate time.Time
+}
+
+//TODO: database functions
