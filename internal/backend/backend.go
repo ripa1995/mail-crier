d8698b47f508cde8b5e6f34b6f57cbb106861d87
ripa1995 <roberto.ripamonti@ymail.com>
2022-10-17 12:14:34 +0200

    feat: basic backend structure

--- internal/backend/backend.go
+++ internal/backend/backend.go
@@ -1,1 +1,24 @@
-package backend;
+package backend
+
+import (
+	"net/smtp"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type SMTPConfig struct {
+	Username string
+	Password string
+	Hostname string
+}
+
+type MongoConfig struct {
+	ConnectionURI string
+	DatabaseName string
+}
+
+type Backend struct {
+	SMTPAuth *smtp.Auth
+	MongoClient *mongo.Client
+}
+
+//TODO: Backend initialization
