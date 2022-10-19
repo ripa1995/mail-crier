9b738329b6043d8f05f68267d39a38c63b791cfe
ripa1995 <roberto.ripamonti@ymail.com>
2022-10-19 16:35:24 +0200

    feat: collection init

--- internal/backend/storage.go
+++ internal/backend/storage.go
@@ -1,27 +1,71 @@
 package backend
 
 import (
+	"context"
 	"time"
+
 	"github.com/google/uuid"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type MailingList struct {
 	DisplayName string
-	Topic string
+	Topic       string
 }
 
 type Subscriber struct {
-	Name    string
-	Surname string
+	Name        string
+	Surname     string
 	DisplayName string
-	Email string
+	Email       string
 }
 
 type Subscription struct {
-	Id uuid.UUID
-	MailingList *MailingList
-	Subscriber *Subscriber
+	Id               uuid.UUID
+	MailingList      *MailingList
+	Subscriber       *Subscriber
 	SubscriptionDate time.Time
 }
 
-//TODO: database functions
+// TODO: database functions
+func InitCollections(ctx context.Context, db *mongo.Database) error {
+	names, err := db.ListCollectionNames(ctx, nil)
+
+	if err != nil {
+		return err
+	}
+
+	if !containsAll(names, collectionNames()) {
+		for _, coll := range collectionNames() {
+			err = db.CreateCollection(ctx, coll)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func containsAll(s []string, values []string) bool {
+	if len(values) > len(s) {
+		return false
+	}
+	count := len(values)
+	for _, str := range s {
+		for _, v := range values {
+			if str == v {
+				count--
+				break
+			}
+		}
+		if count == 0 {
+			return true
+		}
+	}
+	return false
+}
+
+func collectionNames() []string {
+	return []string{"mailing-list", "subscriber"}
+}
